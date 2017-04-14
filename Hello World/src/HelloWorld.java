47f4b1cd38c24c6754473cd49a138c2e304bf778
sonhojin <thsghwls301@naver.com>
2017-04-14 17:06:36 +0900

    "수정"

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,8 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World Java!");
-		System.out.println("Hello Master not hotfix!!");
-		
+		System.out.println("Hello Master not hotfix!!");	
 		System.out.println("Github랑 연결했다.");
 		
 	}
