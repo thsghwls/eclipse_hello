742589c68fcd60d708cf13bcc240de1783c6dd3f
sonhojin <thsghwls301@naver.com>
2017-04-14 16:26:53 +0900

    I am Hotfix

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World Java!");
-		System.out.println("Hello Hotfix!");
+		System.out.println("Hello I am Hotfix!");
 	}
 
 }
