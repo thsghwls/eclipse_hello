f47a17dc2ee941280b93af2c0cefe9b0cec855ff
thsghwls <thsghwls301@naver.com>
2017-04-14 17:07:58 +0900

    Update HelloWorld.java
    ㅅㅅ

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World Java!");
-		System.out.println("Hello Remote!");
+		System.out.println("Hello Remoteㅅㅅㅅㅅㅅㅅ!");
 		System.out.println("Github랑 연결했다.");	
 	}
 
